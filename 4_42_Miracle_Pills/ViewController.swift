d62b77f03cf2733b6cebec93f0a317cf658a2c3a
The M. Do <dominhthe110@gmail.com>
2017-08-30 01:06:54 +0700

    Wed Aug 30 1:06 AM
    Final

--- 4_42_Miracle_Pills/ViewController.swift
+++ 4_42_Miracle_Pills/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
 
     @IBOutlet weak var quanhuyenPicker: UIPickerView!
     @IBOutlet weak var chonPickerBtn: UIButton!
+    @IBOutlet weak var quocgiaLabel: UILabel!
+    @IBOutlet weak var sophoneLabel: UILabel!
+    @IBOutlet weak var quocgiaTF: UITextField!
+    @IBOutlet weak var sophoneTF: UITextField!
+    @IBOutlet weak var buynowBtn: UIButton!
     
-    let quanhuyen = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "Tân Bình", "Tân Phú", "Phú Nhuận", "Nhà Bé", "Bình Chánh", "Hóc Môn", "Chủ Chi", "Bình Thạnh", "Gò Vấp", "Thủ Đức", "Bình Tân", "Cần Giờ"]
+    let quanhuyen = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "Tân Bình", "Tân Phú", "Phú Nhuận", "Nhà Bé", "Bình Chánh", "Hóc Môn", "Củ Chi", "Bình Thạnh", "Gò Vấp", "Thủ Đức", "Bình Tân", "Cần Giờ"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +36,16 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
     @IBAction func chonButtonPressed(_ sender: Any) {
         quanhuyenPicker.isHidden = false
+        quocgiaLabel.isHidden = true
+        quocgiaTF.isHidden = true
+        sophoneLabel.isHidden = true
+        sophoneTF.isHidden = true
+        buynowBtn.isHidden = true
+    
     }
+    @IBAction func buynowBtnPressed(_ sender: Any) {
+    }
+   
 
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -48,6 +62,11 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         chonPickerBtn.setTitle(quanhuyen[row], for: UIControlState.normal)
         quanhuyenPicker.isHidden = true
+        quocgiaLabel.isHidden = false
+        quocgiaTF.isHidden = false
+        sophoneLabel.isHidden = false
+        sophoneTF.isHidden = false
+        buynowBtn.isHidden = false
     }
 }
 
