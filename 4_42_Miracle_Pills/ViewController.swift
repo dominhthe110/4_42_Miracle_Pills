dc7381431f8a87a3e268e7fc43f03e1a8ada1402
The M. Do <dominhthe110@gmail.com>
2017-08-29 17:04:39 +0700

    Tue Aug 29 5:04 PM

--- 4_42_Miracle_Pills/ViewController.swift
+++ 4_42_Miracle_Pills/ViewController.swift
@@ -8,19 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var quanhuyenPicker: UIPickerView!
+    @IBOutlet weak var chonPickerBtn: UIButton!
+    
+    let quanhuyen = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "Tân Bình", "Tân Phú", "Phú Nhuận", "Nhà Bé", "Bình Chánh", "Hóc Môn", "Chủ Chi", "Bình Thạnh", "Gò Vấp", "Thủ Đức", "Bình Tân", "Cần Giờ"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        quanhuyenPicker.dataSource = self
+        quanhuyenPicker.delegate = self
+        
+        
         }
 
-    override func didReceiveMemoryWarning() {
+        override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func chonButtonPressed(_ sender: Any) {
+        quanhuyenPicker.isHidden = false
+    }
 
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return quanhuyen.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return quanhuyen[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        chonPickerBtn.setTitle(quanhuyen[row], for: UIControlState.normal)
+        quanhuyenPicker.isHidden = true
+    }
 }
 
